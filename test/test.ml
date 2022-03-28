e9b9a1ea6233e96e53932a354225ff3c39c009f7
Swrup <swrup@protonmail.com>
2022-03-28 21:57:14 +0200

    change test

--- test/test.ml
+++ test/test.ml
@@ -1,6 +1,18 @@
+let print_emojis emojis =
+  Array.iteri
+    (fun i emoji ->
+      if i mod 80 = 0 then print_newline ();
+      print_string emoji )
+    emojis;
+  print_newline ()
+
 let () =
-  let ar = Emoji.all_emojis |> Array.of_list in
-  for i = 0 to Array.length ar - 1 do
-    if i mod 80 = 0 then print_newline ();
-    print_string ar.(i)
-  done
+  print_emojis Emoji.all_emojis;
+  print_string "Animal & Nature:";
+  print_emojis Emoji.category_animals_and_nature;
+  print_string "Animal-reptile:";
+  print_emojis Emoji.sub_category_animal_reptile;
+  print_string "melting face:";
+  print_newline ();
+  print_string Emoji.melting_face;
+  print_newline ()
