7068c5f9d91862aa6e7ada6180fc45baf22f8df3
Swrup <swrup@protonmail.com>
2022-12-21 21:28:33 +0100

    add count test

--- test/test.ml
+++ test/test.ml
@@ -1,3 +1,11 @@
+let () =
+  (* from https://www.unicode.org/emoji/charts/emoji-counts.html *)
+  assert (Array.length Emoji.category_animals_and_nature = 152);
+  assert (Array.length Emoji.category_smileys_and_emotion = 166);
+  assert (Array.length Emoji.category_symbols = 223);
+  assert (Array.length Emoji.category_people_and_body = 2148);
+  assert (Array.length Emoji.all_emojis = 3664)
+
 let print_emojis emojis =
   Array.iter print_string emojis;
   print_newline ()
