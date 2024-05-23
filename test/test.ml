e0d7b7a19bd6ec96cda8cf2fbbd2041402172251
Swrup <swrup@protonmail.com>
2024-05-24 01:23:20 +0200

    update test to 15.1

--- test/test.ml
+++ test/test.ml
@@ -1,10 +1,10 @@
 let () =
   (* from https://www.unicode.org/emoji/charts/emoji-counts.html *)
-  assert (Array.length Emoji.category_animals_and_nature = 152);
-  assert (Array.length Emoji.category_smileys_and_emotion = 166);
+  assert (Array.length Emoji.category_animals_and_nature = 153);
+  assert (Array.length Emoji.category_smileys_and_emotion = 168);
   assert (Array.length Emoji.category_symbols = 223);
-  assert (Array.length Emoji.category_people_and_body = 2148);
-  assert (Array.length Emoji.all_emojis = 3664)
+  assert (Array.length Emoji.category_people_and_body = 2260);
+  assert (Array.length Emoji.all_emojis = 3782)
 
 let print_emojis emojis =
   Array.iter print_string emojis;
@@ -17,6 +17,8 @@ let () =
   print_emojis Emoji.category_animals_and_nature;
   print_string "Animal-reptile:\n";
   print_emojis Emoji.sub_category_animal_reptile;
-  print_string "melting face:\n";
-  print_string Emoji.melting_face;
-  print_newline ()
+  Printf.printf "melting face:\n%s\n" Emoji.melting_face;
+  Printf.printf "head shaking horizontally:\n%s\n"
+    Emoji.head_shaking_horizontally;
+  Printf.printf "head shaking vertically:\n%s\n" Emoji.head_shaking_vertically;
+  ()
